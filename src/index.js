49b60db36e7b63ebaa8dbc34297c0dfac6f56281
hugopascual <hugo.pascual.adan@upm.es>
2025-09-21 18:16:04 +0200

    base service ready

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import http from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  console.info({ message: 'Hello World Worker received a request!' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
